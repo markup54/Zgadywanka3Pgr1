fef4c172fd7e0834570cbd231767e2e2d6bb8a94
markup54 <marzenakupka@tlen.pl>
2022-09-08 14:50:41 +0200

    losowanie liczby

--- src/com/klasa3P/Main.java
+++ src/com/klasa3P/Main.java
@@ -3,5 +3,17 @@ package com.klasa3P;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Witaj w Javie");
+        //komentarz jednoliniowy
+        /*
+        komentarz wieloliniowy
+         */
+        /**
+         * javadoc
+         */
+        int wylosowana =(int)( Math.random()*100+1);
+                //random() losuje liczbę rzeczywistą z zakresu <0,1)
+        //rzutowanie zawężające
+        double innnaLiczba =wylosowana;//konwersja rozszerzająca
+        System.out.println("Wylosowano "+wylosowana);
     }
 }
