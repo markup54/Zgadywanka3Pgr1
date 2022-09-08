f0ae402d3975b4c3bf217c00192b8abe1bc771a6
markup54 <marzenakupka@tlen.pl>
2022-09-08 15:19:29 +0200

    wyraenie i instrukcje warunkowe if switch

--- src/com/klasa3P/Main.java
+++ src/com/klasa3P/Main.java
@@ -23,5 +23,38 @@ public class Main {
         int wczytana;
         wczytana = klawiatura.nextInt();
         System.out.println("wpisałeś "+wczytana);
+        if (wczytana == wylosowana){
+            System.out.println("gratulacje liczba znaleziona");
+        }
+        else{
+            System.out.println("niestety nie udało się ale ...");
+            int roznica =wylosowana>wczytana ? wylosowana - wczytana : wczytana -wylosowana;
+            //wyrazenie warunkowe zwraca wartosc
+            //warunek ? zwracana wart jeseli warunek jest prawdziwy : zwr wart war fałszywy
+            roznica =roznica/10;//dzielenie całkowite bo roznica jest int
+            switch (roznica){
+                case 0:
+                    System.out.println("Było bardzo blisko");
+                    break;
+                case 1:
+                    System.out.println("Było blisko");
+                    break;
+                case 2:
+                    System.out.println("Trochę brakowało");
+                    break;
+                default:
+                    System.out.println("spróbuj szczęścia gdzie indziej");
+            }
+            //switch jako wyrażenie warunkowe
+            System.out.println(
+                    switch (roznica){
+                        case 0 -> "Było bardzo blisko";
+                        case 1 -> "Było blisko";
+                        case 2 -> "Trochę brakowało";
+                        default -> "Dużo brakowało";
+                    }
+            );
+
+        }
     }
 }
